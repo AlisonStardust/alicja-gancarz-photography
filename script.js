a136b7fabbf7b7101e14945056fb2c4a241d10fe
alisonstardust <zakulisami@o2.pl>
2018-01-28 17:23:16 +0100

    init

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector(".website__button").addEventListener("click", function changeBG() {
+  var i = 0;
+  if (i = 0) {
+    $('#website__parallax-first').css("background-image", "url(img/marysia.jpg)");
+    i++;
+  } else if (i = 1) {
+    $('#website__parallax-first').css("background-image", "url(img/sukienka.jpg)");
+    i++;
+  } else if (i = 2) {
+    $('#website__parallax-first').css("background-image", "url(img/9.jpg)");
+    i++;
+  }
+});
+
+//toggle class?
