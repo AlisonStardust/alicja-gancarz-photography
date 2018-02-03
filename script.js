7a09860a1a6b7d56501fcf4e639344865dcadb37
alisonstardust <zakulisami@o2.pl>
2018-02-03 17:17:30 +0100

    background changes

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+let i = 0;
+
 document.querySelector(".website__button").addEventListener("click", function changeBG() {
-  var i = 0;
-  if (i = 0) {
+  if (i === 0) {
     $('#website__parallax-first').css("background-image", "url(img/marysia.jpg)");
     i++;
-  } else if (i = 1) {
+  } else if (i === 1) {
     $('#website__parallax-first').css("background-image", "url(img/sukienka.jpg)");
     i++;
-  } else if (i = 2) {
+  } else if (i === 2) {
     $('#website__parallax-first').css("background-image", "url(img/9.jpg)");
     i++;
+  } else if (i === 3) {
+    $('#website__parallax-first').css("background-image", "url(img/5.jpg)");
+    i++;
+  } else if (i === 4) {
+    $('#website__parallax-first').css("background-image", "url(img/legia.jpg)");
+    i++;
+  } else if (i === 5) {
+    $('#website__parallax-first').css("background-image", "url(img/ania.jpg)");
+    i++;
+  } else if (i === 5) {
+    $('#website__parallax-first').css("background-image", "url(img/11.jpg)");
+    i++;
+  } else if (i === 6) {
+    i === 0;
   }
 });
-
-//toggle class?
