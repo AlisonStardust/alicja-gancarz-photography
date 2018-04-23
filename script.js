ebb2a6a709c1d1ea2b6ecaa0e43cd5d9923f5cfa
alisonstardust <zakulisami@o2.pl>
2018-04-23 21:58:55 +0200

    updating photos

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var firstParallax = ['img/1.jpg', 'img/bw.jpg', 'img/3.jpg', 'img/11.jpg', 'img/ania.jpg', 'img/legia.jpg', 'img/sukienka.jpg', 'img/9.jpg', 'img/lgbt.jpg'];
-var secondParallax = ['img/portrait/1.jpg', 'img/portrait/pasiut.jpg', 'img/portrait/3.jpg', 'img/portrait/2.jpg', 'img/portrait/4.jpg', 'img/portrait/5.jpg', 'img/portrait/6.jpg',
+var firstParallax = ['img/1.jpg', 'img/tokyo.jpg', 'img/bw.jpg', 'img/3.jpg', 'img/11.jpg', 'img/Damian.jpg', 'img/ania.jpg', 'img/legia.jpg', 'img/sukienka.jpg', 'img/9.jpg', 'img/lgbt.jpg'];
+var secondParallax = ['img/portrait/1.jpg', 'img/portrait/pasiut.jpg', 'img/portrait/ola4.jpg', 'img/portrait/3.jpg', 'img/portrait/2.jpg', 'img/portrait/4.jpg', 'img/portrait/5.jpg', 'img/portrait/6.jpg',
 'img/portrait/7.jpg', 'img/portrait/8.jpg', 'img/portrait/9.jpg'];
 var thirdParallax = ['img/event/1.jpg', 'img/event/2.jpg', 'img/event/3.jpg', 'img/event/4.jpg', 'img/event/5.jpg', 'img/event/6.jpg',
 'img/event/7.jpg', 'img/event/8.jpg', 'img/event/9.jpg', 'img/event/10.jpg', 'img/event/11.jpg', 'img/event/12.jpg', 'img/event/13.jpg', 'img/event/14.jpg'];
 var fourthParallax = ['img/nature/1.jpg', 'img/nature/2.jpg', 'img/nature/3.jpg', 'img/nature/4.jpg', 'img/nature/5.jpg', 'img/nature/6.jpg',
 'img/nature/7.jpg', 'img/nature/8.jpg', 'img/nature/9.jpg', 'img/nature/10.jpg', 'img/nature/11.jpg', 'img/nature/12.jpg', 'img/nature/13.jpg'];
+var fifthParallax = ['img/lights/1.jpg', 'img/lights/Ola.jpg'];
 
 
 let x = 0;
 let x1 = 0;
 let x2 = 0;
 let x3 = 0;
+let x4 = 0;
 let zet;
 
 document.querySelector(".website__button2").addEventListener("click", function changeBG() {
@@ -76,4 +78,20 @@ document.querySelector(".website__button7").addEventListener("click", function c
     x3 = fourthParallax.length-1;
   }
   $('.website__parallax-fourth').css("background-image", "url(" + fourthParallax[x3] + ")");
+});
+
+document.querySelector(".website__button10").addEventListener("click", function changeBG() {
+  x4++;
+  if (x4 >= fifthParallax.length) {
+    x4 = 0;
+  }
+  $('.website__parallax-fifth').css("background-image", "url(" + fifthParallax[x4] + ")");
+});
+
+document.querySelector(".website__button9").addEventListener("click", function changeBG() {
+  x4--;
+  if (x4 < 0) {
+    x4 = fifthParallax.length-1;
+  }
+  $('.website__parallax-fifth').css("background-image", "url(" + fifthParallax[x4] + ")");
 });
